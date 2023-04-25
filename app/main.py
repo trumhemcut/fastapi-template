737054cd2eb2e3d274fae6c88d3adf974393b94f
Phi Huynh <trumhemcut@hotmail.com>
2023-04-25 13:38:56 +0700

    Support CORS #2

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import companies
 from .routers import users
@@ -8,6 +9,16 @@ app = FastAPI()
 app.include_router(companies.router)
 app.include_router(users.router)
 app.include_router(tasks.router)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
