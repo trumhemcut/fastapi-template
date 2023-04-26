b3ceae418e1b3724d70a80a6a24524fbb78ca36b
Phi Huynh <trumhemcut@hotmail.com>
2023-04-26 13:26:33 +0700

    Add delete & query item

--- app/routers/companies.py
+++ app/routers/companies.py
@@ -1,5 +1,5 @@
 import logging
-from fastapi import APIRouter, Depends, Request, status
+from fastapi import APIRouter, Depends, HTTPException, Request, status
 from fastapi_microsoft_identity import requires_auth, validate_scope
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlmodel import select
@@ -8,6 +8,7 @@ from app.db import get_session
 from app.models.company import Company
 
 router = APIRouter()
+
 
 @router.get("/companies", response_model=list[Company])
 @requires_auth
@@ -21,6 +22,22 @@ async def get_companies(request: Request, session: AsyncSession = Depends(get_se
     logging.info('Finished getting companies')
     return companies
 
+
+@router.get("/companies/{company_id}", response_model=Company)
+@requires_auth
+async def get_companies(company_id: int, request: Request, session: AsyncSession = Depends(get_session)):
+    logging.info('Validate scope')
+    validate_scope(request=request, required_scope="Data.Read")
+
+    logging.info(f'Getting a company with id: {company_id}')
+    result = await session.execute(select(Company).where(Company.id == company_id))
+    company = result.scalars().first()
+    
+    if company is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Company not found")
+    
+    logging.info('Finished getting company')
+    return company
 
 @router.post("/companies", status_code=status.HTTP_201_CREATED, response_model=Company)
 @requires_auth
@@ -40,3 +57,22 @@ async def add_company(request: Request, company: Company, session: AsyncSession 
 
     logging.info('Finished adding company')
     return company
+
+
+@router.delete("/companies/{company_id}")
+@requires_auth
+async def get_companies(company_id: int, request: Request, session: AsyncSession = Depends(get_session)):
+    logging.info('Validate scope')
+    validate_scope(request=request, required_scope="Data.Write")
+
+    logging.info(f'Deleting a company with id: {company_id}')
+    result = await session.execute(select(Company).where(Company.id == company_id))
+    company = result.scalars().first()
+    
+    if company is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Company not found")
+    
+    await session.delete(company)
+    await session.commit()
+    
+    logging.info('Deleted company')
